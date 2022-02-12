1168fa387c5548f611a4700007ac47d26a955808
Shimpei Wakida <[shimpei.wakida@gmail.com>
2022-02-12 12:23:24 +0900

    6章まで

--- api/app/controllers/v1/todos_controller.rb
+++ api/app/controllers/v1/todos_controller.rb
@@ -5,13 +5,12 @@ class V1::TodosController < ApplicationController
   end
 
   def create
-    # todo = Todo.new(todo_params)
-    # if todo.save
-    #   render json: todo
-    # else
-    #   render json: todo.errors
-    # end
-    puts params
+    todo = Todo.new(todo_params)
+    if todo.save
+      render json: todo
+    else
+      render json: todo.errors
+    end
   end
 
   def destroy
--- api/app/controllers/v1/users_controller.rb
+++ api/app/controllers/v1/users_controller.rb
@@ -5,7 +5,7 @@ class V1::UsersController < ApplicationController
   end
 
   def create
-    user = Use.new(user_params)
+    user = User.new(user_params)
     if user.save
       render json:user
   else

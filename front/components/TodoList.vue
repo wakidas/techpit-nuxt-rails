0e71eebe0a3e86c39be438db26754063b01862d5
Shimpei Wakida <[shimpei.wakida@gmail.com>
2022-02-12 10:18:46 +0900

    4-2まで

---
+++ front/components/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Todo List
+      <v-spacer></v-spacer>
+      <v-text-firld v-model="search" label="Search" single-line hide-details></v-text-firld>
+    </v-card-title>
+    <v-data-table :headers="headers" :items="todos" :search="search"></v-data-table>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [
+        {
+          title: "test",
+          username: "太郎"
+        }
+      ],
+      search: "",
+      headers: [
+        {
+          text: 'タイトル',
+          align: "left",
+          sortable: false,
+          value: "title"
+        },
+        {
+          text: 'ユーザー名',
+          value: "username"
+        }
+      ],
+    }
+  }
+}
+</script>

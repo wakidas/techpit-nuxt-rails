658217ee625b87f17ac7120350a33c9704e6b758
Shimpei Wakida <[shimpei.wakida@gmail.com>
2022-02-12 17:04:26 +0900

    7-4まで

---
+++ front/plugins/authCheck.js
@@ -0,0 +1,21 @@
+import firebase from "@/plugins/firebase"
+import axios from "@/plugins/axios"
+
+const authCheck = ({ store, redirect }) => {
+  // console.log('[[[[[[[ authCheckの中  ]]]]]]]]]]]');
+  firebase.auth().onAuthStateChanged(async user => {
+    if(user) {
+      const {data} = await axios.get('v1/users', {
+        params: {
+          uid: user.uid,
+        }
+      })
+      // console.log('「「「　ログインしているユーザー: ', data);
+      store.dispatch("auth/setUser", data)
+    } else {
+      store.dispatch("auth/setUser", null)
+    }
+  })
+}
+
+export default authCheck;

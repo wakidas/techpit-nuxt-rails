e57e08fcde00ef165238005bdfd78dbc2f0dcfc2
Shimpei Wakida <[shimpei.wakida@gmail.com>
2022-02-12 11:25:04 +0900

    5-3まで

--- api/app/controllers/v1/todos_controller.rb
+++ api/app/controllers/v1/todos_controller.rb
@@ -5,12 +5,13 @@ class V1::TodosController < ApplicationController
   end
 
   def create
-    todo = Todo.new(todo_params)
-    if todo.save
-      render json: todo
-    else
-      render json: todo.errors
-    end
+    # todo = Todo.new(todo_params)
+    # if todo.save
+    #   render json: todo
+    # else
+    #   render json: todo.errors
+    # end
+    puts params
   end
 
   def destroy

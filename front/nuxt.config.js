967ba807f4f5683e33442aa76cecf88e0a2effc0
Shimpei Wakida <[shimpei.wakida@gmail.com>
2022-02-15 15:34:33 +0900

    9-2まで

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -31,7 +31,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    "@/plugins/authCheck"
+    "@/plugins/authCheck",
+    "@/plugins/vuetify"
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
